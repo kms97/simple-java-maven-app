5e74acf9bbb855da96f1701d9b291b001d61edfb
Kavi Shah <kavshah@deloitte.com.au>
2020-11-23 22:15:11 +1100

    fix deploy script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-scp -i produser.pem /tmp/.auth produser@3.104.120.213:/tmp/.auth
-scp -i produser.pem ./jenkins/deploy/publish.sh produser@3.104.120.213:/tmp/publish.sh
-ssh -i produser.pem produser@3.104.120.213 "/tmp/publish.sh"
+scp -i /opt/produser.pem /tmp/.auth produser@3.104.120.213:/tmp/.auth
+scp -i /opt/produser.pem ./jenkins/deploy/publish.sh produser@3.104.120.213:/tmp/publish.sh
+ssh -i /opt/produser.pem produser@3.104.120.213 "/tmp/publish.sh"
